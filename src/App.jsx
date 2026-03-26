d019eee8dbaecdf014570cc3e15a12fdcce242dd
Vương Quốc Hoàng Thái <vuongquochoangthai@gmail.com>
2026-03-26 19:44:12 +0700

    Sign Up Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
 import './App.css';
 import { useState } from "react";
-
-function TodoList() {
-  const todos = [
-    {id: 1, text: "Learn JSX"},
-    {id: 2, text: "Understand props"},
-    {id: 3, text: "Master state & events"},
-  ];
-
-  return (
-    <div>
-      <h2>My React Todos</h2>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+import SignupForm from './components/SignUpForm';
 
 function App() {
   return (
     <div>
-      <TodoList />
+      <SignupForm />
     </div>
   )
 }
+
+// COMPONENT = Function that returns JSX
 
 export default App
